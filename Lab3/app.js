62103cceeb4c21d55ae63f014735a4ae6ba54a92
Arpit Agnihotri <arpit151604@gmail.com>
2026-09-08 10:32:57 +0530

    10:32

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,19 +1,25 @@
 import http from "http";
 import * as teams from "./pages/teams.js";
+import { parse as parseUrl } from "url";
 
 const PORT = 5000;
 
 const sendJson = (res, statusCode, data) => {
-    res.writeHead(statusCode, {"content-type": "application/json"});
-    res.end(data === 'undefined' ? '' : JSON.stringify(data));
+    res.writeHead(statusCode, {
+        "Content-Type": "application/json"
+    });
+
+    res.end(data === undefined ? "" : JSON.stringify(data));
 };
 
 const parseJSONBody = (req) => {
     return new Promise((resolve, reject) => {
-        let body = '';
+        let body = "";
+
         req.on("data", (chunk) => {
             body += chunk.toString();
         });
+
         req.on("end", () => {
             try {
                 resolve(body ? JSON.parse(body) : {});
@@ -24,16 +30,36 @@ const parseJSONBody = (req) => {
     });
 };
 
-//console.log("Teams:", teams.getAllTeams());
-const server = http.createServer((req, res) => {
-    //if (req.url === "/" && req.method === "GET") {
-    //    const allTeams = teams.getAllTeams();
-    //    res.writeHead(200, { "Content-Type": "application/json" });
-    //    res.end(JSON.stringify(allTeams));
-    //} else 
-    //    res.statusCode = 404;
-    //    res.end("Not Found");
-    //}
+const server = http.createServer(async (req, res) => {
+
+    const { pathname, query } = parseUrl(req.url, true);
+    const { method } = req;
+
+    console.log("pathname:", pathname);
+    console.log("query:", query);
+    console.log("Method:", method);
+
+    if (pathname === "/api/v1/teams" && method === "GET") {
+
+        const allTeams = teams.getAllTeams();
+
+        return sendJson(res, 200, allTeams);
+    }
+
+    else if (pathname === "/api/v1/teams" && method === "POST") {
+
+        const newTeam = await parseJSONBody(req);
+
+        const team = teams.addTeam(newTeam);
+
+        return sendJson(res, 201, team);
+    }
+
+    else {
+
+        res.statusCode = 404;
+        res.end("Not Found");
+    }
 });
 
 server.listen(PORT, () => {
